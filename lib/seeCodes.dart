a70130333ce0e0ee76f16376ed3e116d0809b85b
mohit <mohityadav.my58@gmail.com>
2022-12-07 17:09:55 +0530

    Done

--- lib/seeCodes.dart
+++ lib/seeCodes.dart
@@ -33,13 +33,57 @@ class _SeeCodesState extends State<SeeCodes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.blueGrey[100],
-      appBar: AppBar(elevation: 0,backgroundColor: Colors.white,title:
-      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-        Icon(Icons.message_outlined,color: Colors.orange,size: 40,),
-        Container(decoration: BoxDecoration(color: Colors.grey[200],borderRadius: BorderRadius.circular(5)),width: MediaQuery.of(context).size.width/1.5,padding: EdgeInsets.all(0),child: TextField(decoration: InputDecoration(border: InputBorder.none,hintText: "Search posts",prefixIcon: Icon(Icons.search,color: Colors.black,),suffixIcon: Icon(Icons.info_outline)),)),
-        Icon(Icons.help_outline_rounded,color: Colors.black,),
-        Icon(Icons.account_circle,color: Colors.black,size: 40,)
-      ],),),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: InkWell(
+            onTap: () {
+              Navigator.of(context).pop();
+              // _scaffoldKey.currentState!.openDrawer();
+
+              // if (sideWidth == 0) {
+              //   sideWidth = 300;
+              // } else {
+              //   sideWidth = 0;
+              // }
+              // setState(() {});
+            },
+            child: Icon(
+              Icons.arrow_back_rounded,
+              color: Colors.black,
+            )),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(5)),
+                  padding: EdgeInsets.all(0),
+                  child: TextField(
+                    // autofocus: false,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search posts",
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        suffixIcon: Icon(Icons.info_outline)),
+                  )),
+            ),
+          ],
+        ),
+        actions: [
+          Icon(
+            Icons.account_circle,
+            color: Colors.black,
+            size: 40,
+          ),
+          SizedBox(width: 16,)
+        ],
+      ),
       body: ListView.builder(shrinkWrap: true,primary: false,itemCount: list.length,itemBuilder: (BuildContext context,int index){
         return index==0?Padding(
           padding: const EdgeInsets.only(left: 24.0,right: 24,top: 24),
